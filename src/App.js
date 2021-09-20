89d423d25c3c4e7236dfc5078d04acdddc7f9af4
abdelaziz123456 <abdelazizmohamed1233@gmail.com>
2021-09-20 07:44:56 +0200

    add modal

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { Fragment } from "react";
 import Meals from "./components/Meals/Meals";
 import Header from "./components/UI/Header";
-
+import Cart from "./components/Cart/Cart";
 
 function App() {
   return (
     <Fragment>
+      <Cart/>
     <Header/>
     <main>
     <Meals/>
